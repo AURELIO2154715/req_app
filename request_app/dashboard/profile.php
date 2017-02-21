d093ebfb1af77bc197be0ab1bc966ccb893c2b20
maiiradie <2159296@slu.edu.ph>
2017-02-21 21:22:39 +0800

    dinagdagan ko ng feature para sa accounting yung profile

--- request_app/dashboard/profile.php
+++ request_app/dashboard/profile.php
@@ -11,8 +11,12 @@
             $user_type = $_SESSION['user_type'];
             $user_info = "Select users.id, username, user_type, password,firstname, middlename, lastname from users inner join user_details on users.id = user_id where users.id = '$user_id';";
             
-            $requests = "SELECT * FROM request_form WHERE requested_by = '$user_id' ORDER BY created_at DESC";
-
+            $requests = "SELECT * FROM request_form WHERE requested_by = '$user_id' ORDER BY created_at DESC"; 
+        
+            $req_accounting = "SELECT request_id, received_by, request_status, use_of_item, date_needed,request_form.created_at FROM request_form INNER JOIN status_report ON request_form_id = requested_by WHERE received_by = '6' and request_status = 'approved'"; 
+            
+            $request_accountingQuery = mysqli_query($conn,$req_accounting) or die(mysqli_error($conn));
+        
             $requestQuery = mysqli_query($conn,$requests) or die(mysqli_error($conn));
                
             $user_infoQ = mysqli_query($conn,$user_info) or die(mysqli_error($conn)); 
@@ -30,27 +34,40 @@
             echo "<b>First Name: </b>" . $user['firstname'] . "<br>";
             echo "<b>Middle Name: </b>" . $user['middlename'] . "<br>";
             echo "<b>Last Name: </b>" . $user['lastname'] . "<br>";
-        ?>
         
-        <h2>Requested Items</h2>
-        <table style="width:50%" border="1">
-            <tr>
-                <th>Request No.</th>
-                <th>Request Description</th> 
-                <th>Status</th>
-                <th>Date Needed</th>
-                <th>Action</th>
-            </tr>   
-        <?php
-            while ($log = mysqli_fetch_array($requestQuery)) {
-                echo "<tr><td> RF 00" . $log['request_id'] . "</td>";
-			  	echo "<td>" . $log['use_of_item'] . "</td>";
-			  	echo "<td>" . $log['request_status'] . "</td>";
-			  	echo "<td>" . $log['date_needed'] . "</td>";
-			  	echo "<td><a href='request_details.php?request_id=" . $log['request_id'] . "'>View Details</a></td></tr>"; 
+            if ($user['user_type'] == "scis"){
+                echo "<h2>Requested Items</h2>" . "<table style='width:50%' border='1'>";
+                echo"<tr><th>Request No.</th>";
+                echo "<th>Request Description</th>";
+                echo "<th>Status</th>";
+                echo "<th>Date Needed</th>";
+                echo "<th>Action</th></tr>" ;  
+
+                while ($log = mysqli_fetch_array($requestQuery)) {
+                    echo "<tr><td> RF 00" . $log['request_id'] . "</td>";
+                    echo "<td>" . $log['use_of_item'] . "</td>";
+                    echo "<td>" . $log['request_status'] . "</td>";
+                    echo "<td>" . $log['date_needed'] . "</td>";
+                    echo "<td><a href='request_details.php?request_id=" . $log['request_id'] . "'>View Details</a></td></tr>"; 
+                }
+                
+            } else {
+                echo "<h2>Request Log</h2>" . "<table style='width:50%' border='1'>";
+                echo"<tr><th>Request No.</th>";
+                echo "<th>Use for</th>" ;
+                echo "<th>Status</th>";
+                echo "<th>Date Needed</th>";
+                echo "<th>Action</th></tr>";
+               
+                while ($log = mysqli_fetch_array($request_accountingQuery)){
+                    echo "<tr><td> RF 00" . $log['request_id'] . "</td>";
+                    echo "<td>" . $log['use_of_item'] . "</td>";
+                    echo "<td>" . $log['request_status'] . "</td>";
+                    echo "<td>" . $log['date_needed'] . "</td>";
+                    echo "<td><a href='request_details.php?request_id=" . $log['request_id'] . "'>View Details</a></td></tr>"; 
+                }
             }
         ?>
-        </table>
         
     </body>
 </html>
